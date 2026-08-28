627a318803e2aa6acc8866d8353797e376e97c95
ParameswaranSajeenthiran <psajeendran@gmail.com>
2026-08-28 16:29:18 +0530

    Add missing files

---
+++ dpdp-accelerator/components/org.wso2.dpdp.accelerator.complaint.mgt.service/src/main/java/org/wso2/dpdp/accelerator/complaint/mgt/service/internal/ComplaintServiceComponent.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright (c) 2026, WSO2 LLC. (https://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.dpdp.accelerator.complaint.mgt.service.internal;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.wso2.dpdp.accelerator.complaint.mgt.dao.ComplaintDAOProvider;
+import org.wso2.dpdp.accelerator.complaint.mgt.service.ComplaintAttachmentService;
+import org.wso2.dpdp.accelerator.complaint.mgt.service.ComplaintEventService;
+import org.wso2.dpdp.accelerator.complaint.mgt.service.ComplaintService;
+import org.wso2.dpdp.accelerator.complaint.mgt.service.impl.ComplaintAttachmentServiceImpl;
+import org.wso2.dpdp.accelerator.complaint.mgt.service.impl.ComplaintEventServiceImpl;
+import org.wso2.dpdp.accelerator.complaint.mgt.service.impl.ComplaintServiceImpl;
+
+/**
+ * Constructs the complaint service graph and publishes its public OSGi service contracts,
+ * mirroring {@code EventNotificationServiceComponent} in the event-notifications module - this
+ * bundle previously ran as a plain (non-OSGi) jar, with complaint.mgt.endpoint constructing
+ * {@code ComplaintServiceImpl}/{@code ComplaintEventServiceImpl}/{@code
+ * ComplaintAttachmentServiceImpl} directly via their no-arg constructors instead of resolving
+ * them as OSGi services.
+ */
+@Component(
+        name = "org.wso2.dpdp.accelerator.complaint.mgt.service.internal.ComplaintServiceComponent",
+        immediate = true
+)
+public class ComplaintServiceComponent {
+
+    private static final Log LOG = LogFactory.getLog(ComplaintServiceComponent.class);
+
+    private volatile ComplaintDAOProvider daoProvider;
+
+    private ServiceRegistration<ComplaintService> complaintServiceRegistration;
+    private ServiceRegistration<ComplaintEventService> complaintEventServiceRegistration;
+    private ServiceRegistration<ComplaintAttachmentService> complaintAttachmentServiceRegistration;
+
+    @Activate
+    protected void activate(ComponentContext context) {
+
+        ComplaintService complaintService = new ComplaintServiceImpl(
+                daoProvider.getComplaintDAO(), daoProvider.getComplaintEventDAO());
+        ComplaintEventService complaintEventService = new ComplaintEventServiceImpl(
+                daoProvider.getComplaintEventDAO(), daoProvider.getComplaintDAO(), complaintService);
+        ComplaintAttachmentService complaintAttachmentService = new ComplaintAttachmentServiceImpl(
+                daoProvider.getComplaintAttachmentDAO(), daoProvider.getComplaintEventDAO(), complaintService);
+
+        complaintServiceRegistration = context.getBundleContext().registerService(
+                ComplaintService.class, complaintService, null);
+        complaintEventServiceRegistration = context.getBundleContext().registerService(
+                ComplaintEventService.class, complaintEventService, null);
+        complaintAttachmentServiceRegistration = context.getBundleContext().registerService(
+                ComplaintAttachmentService.class, complaintAttachmentService, null);
+        LOG.debug("Complaint services are activated successfully.");
+    }
+
+    @Deactivate
+    protected void deactivate(ComponentContext context) {
+
+        unregister(complaintAttachmentServiceRegistration);
+        complaintAttachmentServiceRegistration = null;
+        unregister(complaintEventServiceRegistration);
+        complaintEventServiceRegistration = null;
+        unregister(complaintServiceRegistration);
+        complaintServiceRegistration = null;
+        LOG.debug("Complaint services are deactivated.");
+    }
+
+    @Reference(
+            service = ComplaintDAOProvider.class,
+            cardinality = ReferenceCardinality.MANDATORY,
+            policy = ReferencePolicy.STATIC,
+            unbind = "unsetComplaintDAOProvider"
+    )
+    protected void setComplaintDAOProvider(ComplaintDAOProvider daoProvider) {
+
+        this.daoProvider = daoProvider;
+        ComplaintServiceDataHolder.getInstance().setDaoProvider(daoProvider);
+    }
+
+    protected void unsetComplaintDAOProvider(ComplaintDAOProvider daoProvider) {
+
+        if (this.daoProvider == daoProvider) {
+            this.daoProvider = null;
+        }
+        ComplaintServiceDataHolder dataHolder = ComplaintServiceDataHolder.getInstance();
+        if (dataHolder.getDaoProvider() == daoProvider) {
+            dataHolder.setDaoProvider(null);
+        }
+    }
+
+    private static void unregister(ServiceRegistration<?> registration) {
+        if (registration != null) {
+            registration.unregister();
+        }
+    }
+}
